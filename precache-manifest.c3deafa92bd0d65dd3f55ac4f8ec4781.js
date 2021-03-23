9836c492e0df6865b75d106963df97c7685b9882
Gufron Atokhojaev <atohodgufron@gmail.com>
2021-03-24 00:45:52 +0100

    Updates

---
+++ precache-manifest.c3deafa92bd0d65dd3f55ac4f8ec4781.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1bbd0aab3128859704a097380c79c6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "505f38e5d996ed090230",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "a9a7ac3b3f16edc25893",
+    "url": "/static/js/2.88408be9.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.88408be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505f38e5d996ed090230",
+    "url": "/static/js/main.86d9eb11.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "b94c99f7ad8d3702ac21ad315f4d9932",
+    "url": "/static/media/bechtle.b94c99f7.png"
+  },
+  {
+    "revision": "8078cb34fc707f623f5e6768c37946d2",
+    "url": "/static/media/concardis.8078cb34.png"
+  },
+  {
+    "revision": "47808fa520309963e39707c57fc1d1b9",
+    "url": "/static/media/gufronato.47808fa5.png"
+  },
+  {
+    "revision": "bc9ef478a4b72adf34cee26b0b56b986",
+    "url": "/static/media/kisters.bc9ef478.png"
+  },
+  {
+    "revision": "9be43f15144533c3d5143c6af7bc0a14",
+    "url": "/static/media/nets.9be43f15.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
